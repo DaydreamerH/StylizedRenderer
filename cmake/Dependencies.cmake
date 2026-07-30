469a678439fb49db81b52cadfab16354ca1278d4
DaydreamerH <332068335@qq.com>
2026-07-30 16:23:09 +0800

    build: add gltf import dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -7,6 +7,13 @@ set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 
+set(SIMDJSON_DEVELOPER_MODE OFF CACHE BOOL "" FORCE)
+
+set(FASTGLTF_ENABLE_TESTS OFF CACHE BOOL "" FORCE)
+set(FASTGLTF_ENABLE_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(FASTGLTF_ENABLE_DOCS OFF CACHE BOOL "" FORCE)
+set(FASTGLTF_COMPILE_AS_CPP20 ON CACHE BOOL "" FORCE)
+
 FetchContent_Declare(
     glfw
     GIT_REPOSITORY https://github.com/glfw/glfw.git
@@ -21,15 +28,47 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE
 )
 
+FetchContent_Declare(
+    simdjson
+    GIT_REPOSITORY https://github.com/simdjson/simdjson.git
+    GIT_TAG v3.9.4
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_Declare(
+    fastgltf
+    GIT_REPOSITORY https://github.com/spnda/fastgltf.git
+    GIT_TAG v0.8.0
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_Declare(
+    stb
+    GIT_REPOSITORY https://github.com/nothings/stb.git
+    GIT_TAG 31c1ad37456438565541f4919958214b6e762fb4
+)
+
 FetchContent_MakeAvailable(
     glfw
     glm
+    simdjson
+    fastgltf
+    stb
 )
 
-set_target_properties(glfw PROPERTIES
-    FOLDER "Dependencies"
+add_library(stylized_stb INTERFACE)
+
+target_include_directories(stylized_stb
+    INTERFACE
+        ${stb_SOURCE_DIR}
 )
 
-set_target_properties(glm PROPERTIES
-    FOLDER "Dependencies"
+set_target_properties(
+    glfw
+    glm
+    simdjson
+    fastgltf
+    stylized_stb
+    PROPERTIES
+        FOLDER "Dependencies"
 )
