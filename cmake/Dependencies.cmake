2a7c624a4294e1235f561d4717a15e9884446eb2
DaydreamerH <332068335@qq.com>
2026-07-31 17:14:48 +0800

    build: add assimp

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -7,6 +7,14 @@ set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
 
+set(ASSIMP_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE BOOL "" FORCE)
+set(ASSIMP_BUILD_SAMPLES OFF CACHE BOOL "" FORCE)
+set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT OFF CACHE BOOL "" FORCE)
+set(ASSIMP_INSTALL OFF CACHE BOOL "" FORCE)
+set(ASSIMP_WARNINGS_AS_ERRORS OFF CACHE BOOL "" FORCE)
+set(ASSIMP_INJECT_DEBUG_POSTFIX OFF CACHE BOOL "" FORCE)
+
 FetchContent_Declare(
     glfw
     GIT_REPOSITORY https://github.com/glfw/glfw.git
@@ -22,6 +30,13 @@ FetchContent_Declare(
 )
 
 FetchContent_Declare(
+    assimp
+    GIT_REPOSITORY https://github.com/assimp/assimp.git
+    GIT_TAG v6.0.5
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_Declare(
     stb
     GIT_REPOSITORY https://github.com/nothings/stb.git
     GIT_TAG 31c1ad37456438565541f4919958214b6e762fb4
@@ -30,6 +45,7 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(
     glfw
     glm
+    assimp
     stb
 )
 
@@ -43,6 +59,7 @@ target_include_directories(stylized_stb
 set_target_properties(
     glfw
     glm
+    assimp
     stylized_stb
     PROPERTIES
         FOLDER "Dependencies"
